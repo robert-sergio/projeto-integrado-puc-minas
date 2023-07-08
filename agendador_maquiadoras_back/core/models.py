f5aa9bca1232daf90d61e3a9adff69a86b6c8519
Robert <robert.sergio.eng@gmail.com>
2023-07-08 15:33:34 -0300

    config models

--- agendador_maquiadoras_back/core/models.py
+++ agendador_maquiadoras_back/core/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
-class Pessoa:
+class Cliente(models.Model):
     nome = models.CharField(max_length=250, null=False)
     endereco = models.CharField(max_length=500, null=False)
     telefone = models.CharField(max_length=25)
     whatsapp = models.CharField(max_length=25)
-
-class Cliente(Pessoa):
     email = models.EmailField(max_length=100, null=False)
     senha = models.CharField(max_length=24, null=False)
 
-class Profissional(Pessoa):
+class Profissional(models.Model):
+    nome = models.CharField(max_length=250, null=False)
+    endereco = models.CharField(max_length=500, null=False)
+    telefone = models.CharField(max_length=25)
+    whatsapp = models.CharField(max_length=25)
     cpf = models.CharField(max_length=15, null=False)
     senha = models.CharField(max_length=24, null=False)
 
@@ -20,18 +22,19 @@ class Profissional(Pessoa):
     instagram = models.CharField(max_length=500, null=True)
     facebook = models.CharField(max_length=500, null=True)
 
-class Agenda:
+class Agenda(models.Model):
     id_profissional = models.IntegerField()
     data = models.DateField(null=True)
     hora = models.CharField(max_length=4, null=True)
     status = models.BooleanField(default=True)
 
-class Atendimentos:
+class Atendimentos(models.Model):
     id_cliente = models.IntegerField()
     id_profissional = models.IntegerField()
     id_agenda = models.IntegerField()
+
     cidade_atendimento = models.CharField(max_length=500)
-    valor = models.DecimalField(decimal_places=2)
+    valor = models.DecimalField(decimal_places=2, max_digits=6)
     status = models.BooleanField()
 
     msg = models.CharField(max_length=100)
