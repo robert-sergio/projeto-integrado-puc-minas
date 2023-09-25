61329eac623d61d9f7e311d6fde2e77b4add6a26
Robert <robert.sergio.eng@gmail.com>
2023-09-25 13:09:50 -0300

    made 2 tests

--- agendador_maquiadoras_back/core/tests.py
+++ agendador_maquiadoras_back/core/tests.py
@@ -1,4 +1,24 @@
 from django.test import TestCase
+import pytest
 from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+
 
 # Create your tests here.
+class ClienteTestCase(TestCase):
+    def setUp(self):
+        self.email_cliente_cadastrado = 'oi@oi.com.br'
+        self.email_cliente_nao_cadastrado = 'oi@nao_cadastrado.com.br'
+
+        self.senha_teste = 'abc123'
+        self.nome_cliente_cadastrado = 'cliente_cadastrado'
+
+        Cliente.objects.create(nome=self.nome_cliente_cadastrado, email=self.email_cliente_cadastrado, senha=self.senha_teste)
+    
+    def test_cliente_cadastrado(self):
+        existe = Cliente.objects.get(email=self.email_cliente_cadastrado, senha=self.senha_teste)
+        assert(existe.nome==self.nome_cliente_cadastrado)
+    
+    def test_cliente_nao_cadastrado(self):
+        with pytest.raises(ObjectDoesNotExist):
+            existe = Cliente.objects.get(email=self.email_cliente_nao_cadastrado, senha=self.senha_teste)
